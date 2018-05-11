48828e0d114c0cf61e14f66e8439835a5907cded
Pritish Thakkar <prince.thakkar001@gmail.com>
2018-05-11 12:29:17 +0530

    Dynamic Programming

---
+++ LeetCode/64. Minimum Path Sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int n, m;
+        n = grid.size();
+        m = grid[0].size();
+        int dp[n][m];
+        for(int i = 0 ; i < n ; i ++){
+            if(!i){
+                dp[i][0] = grid[i][0];
+            }else{
+                dp[i][0] = grid[i][0] + dp[i-1][0];
+            }
+        }
+        for(int j = 1 ; j < m ;j ++){
+            dp[0][j] = grid[0][j] + dp[0][j-1];
+        }
+        for(int i = 1 ; i < n ; i++){
+            for(int j = 1 ; j < m ; j++){
+                dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[n-1][m-1];
+    }
+};
