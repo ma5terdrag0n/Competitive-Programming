2be0cf0c945cb4e341ac8cb6f842d825de21b330
Pritish Thakkar <prince.thakkar001@gmail.com>
2018-05-16 11:09:35 +0530

    Hard Binary Search

---
+++ LeetCode/154. Find Minimum in Rotated Sorted Array II.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findMin(vector<int>& a) {
+        int lo, hi;
+        lo= 0;
+        hi = a.size()-1;
+        int n = a.size();
+        while(lo < hi){
+            int mid = (lo + hi) / 2;
+            if(a[mid] > a[hi]){
+                lo = mid+1;
+            }else if(a[mid] < a[hi]){
+                hi = mid;
+            }
+            else {
+                hi--;
+            }
+        }
+        return a[lo];
+    }
+};
