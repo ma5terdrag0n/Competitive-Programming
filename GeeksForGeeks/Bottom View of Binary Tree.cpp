53f14fda33de9a1e2fd9f219f949c807780d7aaa
Pritish Thakkar <prince.thakkar001@gmail.com>
2018-07-04 17:37:31 +0530

    Create Bottom View of Binary Tree.cpp

---
+++ GeeksForGeeks/Bottom View of Binary Tree.cpp
@@ -0,0 +1,16 @@
+void helper(Node* root, int start){
+    if(!root)return;
+    m[start] = root->data;
+    helper(root->left, start-1);
+    helper(root->right, start+1);
+}
+
+void bottomView(Node *root)
+{
+    m.clear();
+   // Your Code Here
+   helper(root, 0);
+   for(map<int, int>::iterator it = m.begin() ; it!=m.end() ;it++){
+       cout << it->second << " ";
+   }
+}
